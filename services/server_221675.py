bc1b9148d62ee326c209e245c4d34a9602e4f428
Mikhail <misha.shabalin@mail.ru>
2023-05-20 13:55:58 +0300

    Add homepage

--- services/server_221675.py
+++ services/server_221675.py
@@ -6,6 +6,17 @@ import tensorflow as tf
 app = Flask("Image classifier")
 resnet = tf.keras.applications.ResNet101()
 
+with open('../data/imgnet_cats_ru.txt', encoding='utf-8') as f:
+    cats = f.readlines()
+
+cats_ru = [s.rstrip() for s in cats]
+
+
+@app.route("/")
+def home():
+    """function docstring"""
+    return "Home page"
+
 
 if __name__ == "__main__":
     app.run(port=1675)
